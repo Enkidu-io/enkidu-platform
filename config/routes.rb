b764f3cc8a280c28e7552e419945ff6d22bc5ac6
anantdimri96 <anant.dimri96@gmail.com>
2018-02-23 16:33:39 +0530

    first push after bids and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   devise_for :users
-  devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+    resources :bids
 end
