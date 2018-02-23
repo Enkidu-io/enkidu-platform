f5debc44399a18c0865704a3bcf409075d3dad18
a3y3 <sohamssd@gmail.com>
2018-02-23 17:51:17 +0530

    Integrate Material UI

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,23 @@ ActiveRecord::Schema.define(version: 20180223113628) do
     t.integer "resolution_id"
   end
 
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
   create_table "payment_gateways", force: :cascade do |t|
     t.integer "project_id"
     t.float "price_rate"
