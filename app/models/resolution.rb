a78f5687a6774f7735981d3409df4808a611465a
Tejas <tejas.tholpadi7@gmail.com>
2018-02-23 18:46:13 +0530

    resolution seeds

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,2 +1,5 @@
 class Resolution < ApplicationRecord
+
+	validates_presence_of :name
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Resolution.create(name: "Add new Collaborator")
+Resolution.create(name: "Remove Collaborator")
+Resolution.create(name: "Dilute")
