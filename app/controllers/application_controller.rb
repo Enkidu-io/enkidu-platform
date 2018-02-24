a5d57534a3a7cfb35c4aa8827466f9119d3d1535
Tejas <tejas.tholpadi7@gmail.com>
2018-02-24 15:36:27 +0530

    layout by resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && (resource_name == :user)  && action_name != 'edit' && action_name != 'update'
+      "homepages"
+    else
+      "application"
+    end
+  end
 end
