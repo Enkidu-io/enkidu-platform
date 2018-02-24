df2e67f057da7546292a8ae8af48d01923f7ec45
a3y3 <sohamssd@gmail.com>
2018-02-24 13:45:54 +0530

    Create a smart contract on successful votes in bid_details and send notifications for approval

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223154032) do
+ActiveRecord::Schema.define(version: 20180224071520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180223154032) do
     t.datetime "last_clicked"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "notification_description"
   end
 
   create_table "payment_gateways", force: :cascade do |t|
---
+++ lib/NotificationDescription.rb
@@ -0,0 +1,35 @@
+class NotificationDescription
+	def getDescription(type, to_leader, user_email, project_title)
+		if(type == 1)
+			if(to_leader == true)
+				"A smart contract for user #{user_email} is awaiting your approaval."
+			else
+				"Please confirm you would like to sign this smart contract."
+			end
+		elsif(type == 2)
+			if(to_leader == true)
+				"Please confirm user #{user_email} to be added to the list of collaborators for project #{project_title}"
+			else
+				"Please confirm you'd like to be added to the list of collaborators for project #{project_title}"
+			end
+		elsif(type == 3)
+			if(to_leader == true)
+				"Please confirm user #{user_email} to be removed from the list of collaborators for project #{project_title}"
+			else
+				"Please confirm you'd like to be removed from the list of collaborators for project #{project_title}"
+			end
+		elsif(type == 4)
+			if(to_leader == true)
+				"Please confirm your vote for diluting project #{project_title}"
+			else
+				"Please confirm your vote for diluting project #{project_title}"
+			end
+		elsif(type == 5)
+			if(to_leader == true)
+				"Vote for Smart contract for adding #{user_email} to #{project_title} failed"
+			else
+				"Vote for Smart Contract for adding you to #{project_title} failed"
+			end
+		end
+	end
+end
