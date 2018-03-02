a28452cf23833e1712c24143d9e0c93180c65d10
9591355172 <abhijeet428001@gmail.com>
2018-03-02 14:00:25 +0530

    button position

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :bids
 
   get 'dashboard' => 'dashboards#index'
+  get 'browse' => 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'projects#index'
 end
