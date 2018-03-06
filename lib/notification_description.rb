c61d3973f35317c041e1d4c3484047169efe82ba
Tejas <tejas.tholpadi7@gmail.com>
2018-03-06 21:00:58 +0530

    refactored notifications

--- lib/notification_description.rb
+++ lib/notification_description.rb
@@ -1,39 +1,39 @@
 class NotificationDescription
-	def getDescription(type, to_member, user_email, project_title, bid_amount)
+	def self.getDescription(type, to_member, variables)
 		if(type == 1)
 			#Smart Contract
 			if(to_member == true)
-				"A smart contract for user #{user_email} is awaiting your approval."
+				"A smart contract for user #{variables[:full_name]} is awaiting your approval."
 			else
 				"Please confirm you would like to sign this smart contract."
 			end
 		elsif(type == 2)
 			#Add collaborator
 			if(to_member == true)
-				"Please confirm user #{user_email} to be added to the list of collaborators for project #{project_title}. The bid placed is #{bid_amount}"
+				"Please confirm user #{variables[:full_name]} to be added to the list of collaborators for project #{variables[:title]}."
 			else
-				"Please confirm you'd like to be added to the list of collaborators for project #{project_title}"
+				"Please confirm you'd like to be added to the list of collaborators for project #{variables[:title]}."
 			end
 		elsif(type == 3)
 			#Remove collaborator
 			if(to_member == true)
-				"Please confirm your vote for removal of user #{user_email} to be removed from the list of collaborators for project #{project_title}"
+				"Please confirm your vote for removal of user #{variables[:full_name]} to be removed from the list of collaborators for project #{variables[:title]}."
 			else
-				"Please confirm you'd like to be removed from the list of collaborators for project #{project_title}"
+				"Please confirm you'd like to be removed from the list of collaborators for project #{variables[:title]}."
 			end
 		elsif(type == 4)
 			#Vote Dilution
 			if(to_member == true)
-				"Please confirm your vote for diluting project #{project_title} for #{bid_amount}"
+				"Please confirm your vote for diluting project #{variables[:title]}."
 			else
-				"Please confirm your vote for diluting project #{project_title}"
+				"Please confirm your vote for diluting project #{variables[:title]}."
 			end
 		elsif(type == 5)
 			#Smart contract fail
 			if(to_member == true)
-				"Vote for Smart contract for adding #{user_email} to #{project_title} failed"
+				"Vote for Smart contract for adding #{variables[:full_name]} to #{variables[:title]} failed."
 			else
-				"Vote for Smart Contract for adding you to #{project_title} failed"
+				"Vote for Smart Contract for adding you to #{variables[:title]} failed."
 			end
 		end
 	end
