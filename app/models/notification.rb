f5bf5bf71843cb7ac7c9487e702d4f1a00702e65
9591355172 <abhijeet428001@gmail.com>
2018-03-07 15:57:58 +0530

    Notifications seperate page

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,9 @@ class Notification < ApplicationRecord
 	belongs_to :user
 	belongs_to :notification_type
 	validates_presence_of :user_id, :notification_type_id, :notification_description
+
+
+	def group_by_criteria
+  		created_at.to_date.to_s(:db)
+  	end
 end
